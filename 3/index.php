bb95aa502730b31f04229b0160f7d36a34fc0184
tuanpembual <andro.medh4@gmail.com>
2014-06-06 11:41:38 +0700

    ready to mysql

---
+++ 3/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>WarSi | Warung Aplikasi BlankOn</title>
+  </head>
+  <body>
+ini jalan kagak?<br>
+<center>
+  <form action="insert.php" method="post">
+  Firstname: <input type="text" name="firstname">
+  Lastname: <input type="text" name="lastname">
+  Age: <input type="text" name="age">
+  <input type="submit">
+  </form>
+
+</center>
+<?php
+// Create connection
+$con=mysqli_connect("localhost","root","alpa","mesin");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+  //echo "sukses";
+}
+
+//mysqli_query($con,"INSERT INTO Persons (Fistname, LastName, Age) VALUES ('Peter','Grifin',35)");
+//mysqli_close($con);
+
+// create database
+// $sql = "CREATE DATABASE my_db";
+// if (mysqli_query($con,$sql)) {
+//   echo "database created";
+// } else {
+//   echo "error :" . mysqli_error($con);
+// }
+
+// create table
+// $sql = "CREATE TABLE Persons (
+//   PID INT NOT NULL AUTO_INCREMENT,
+//   PRIMARY KEY (PID),
+//   FirsName CHAR(30),
+//   LastName CHAR(30),
+//   Age INT
+// )";
+
+// excute quary
+// if (mysqli_query($con,$sql)) {
+//   echo "Table terbuat";
+// } else {
+//   echo "error table: " . mysqli_error($con);
+// }
+
+// db lama
+// $host = "localhost";
+// $user = "root";
+// $pass = "alpa";
+// $db = "mesin";
+// $konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! ');
+// mysql_select_db($db);
+
+?> 
+
+  </body>
+</html>
