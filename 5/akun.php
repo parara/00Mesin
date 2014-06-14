8b2bd685116a1ac76f939cd4ddd0efdbc00bcced
tuanpembual <andro.medh4@gmail.com>
2014-06-14 09:08:26 +0700

    dynamic monipulasi database

--- 5/akun.php
+++ 5/akun.php
@@ -1,15 +1,11 @@
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
-  # code...
   header('location:login.php');
 } else {
-  # code...
   $username= $_SESSION['username'];
 }
 require_once("connect.php");
-//$query = mysql_query("SELECT * FROM pengguna WHERE username = '$username'");
-//$hasil = mysql_fetch_array($query);
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,32 +21,50 @@ $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   if (empty($_POST["name"])) {
-     $nameErr = "Name is required";
-   } else {
-     test_input($_POST["name"],$username);
-   }
+  if (empty($_POST["firstname"])) {
+    $nameErr = "Name is required";
+  } else {
+    $kolom = "firstname";
+    test_input($_POST["firstname"],$username,$kolom);
+  }
+  if (empty($_POST["lastname"])) {
+    $nameErr = "Name is required";
+  } else {
+    $kolom = "lastname";
+    test_input($_POST["lastname"],$username,$kolom);
+  }
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $kolom = "email";
+    test_input($_POST["email"],$username,$kolom);
+  }
+  if (empty($_POST["desk"])) {
+    $desk = "";
+  } else {
+    $kolom = "desk";
+    test_input($_POST["desk"],$username,$kolom);
+  }
+
+   // if (empty($_POST["gender"])) {
+   //   $genderErr = "Gender is required";
+   // } else {
+   //   test_input($_POST["gender"],$username);
+   // }
 }
 
-// function test_input($data) {
-//    $data = trim($data);
-//    $data = stripslashes($data);
-//    $data = htmlspecialchars($data);
-//    return $data;
-// }
-
-function test_input($inputan,$username) {
+function test_input($inputan,$username,$kolom) {
   $isi = $inputan;
   $ygpunya = $username;
-  $con = mysql_query("UPDATE pengguna SET firstname='$isi' WHERE username = '$ygpunya'") or die(mysql_error());
+  $bagian = $kolom;
+  // echo "$bagian";
+  // kolom apa yang disi
+  $con = mysql_query("UPDATE pengguna SET $bagian='$isi' WHERE username = '$ygpunya'") or die(mysql_error());
   if($con) {
     echo "successful";
   }else {
     echo "error";
   }
-  echo "$inputan satu <br>"; //mahyudin pagi
-  echo "$ygpunya dua <br>"; //mahyudin
-  echo "$username"; //pagi
   return;
 }
 ?>
@@ -58,16 +72,31 @@ function test_input($inputan,$username) {
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-   Name: <input type="text" name="name">
+   Firstname: <input type="text" name="firstname">
    <span class="error">* <?php echo $nameErr;?></span>
    <br><br>
+   Lastname: <input type="text" name="lastname">
+   <span class="error">* <?php echo $nameErr;?></span>
+   <br><br>
+   E-mail: <input type="text" name="email">
+   <span class="error">* <?php echo $emailErr;?></span>
+   <br><br>
+   Website: <input type="text" name="desk">
+   <span class="error"><?php echo $websiteErr;?></span>
+   <br><br>
+   Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+   <br><br>
+   <!-- Gender:
+   <input type="radio" name="gender" value="female">Female
+   <input type="radio" name="gender" value="male">Male
+   <span class="error">* <?php echo $genderErr;?></span>
+   <br><br> -->
    <input type="submit" name="submit" value="Submit">
 </form>
 
 <?php
 echo "<h2>Selamat datang, $username </h2>";
-echo "<h2>Your Input:</h2>";
-echo $name;
+//echo "<h2>Your Input:</h2>";
 ?>
 
 <br>
