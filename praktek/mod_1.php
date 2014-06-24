f202168f6fc7a77a675d3e0c7fed959e6044c6e3
tuanpembual <andro.medh4@gmail.com>
2014-06-24 08:57:04 +0700

    praktek modul akakom

---
+++ praktek/mod_1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+  <title>Variable</title>
+</head>
+<body>
+  <?php
+  $nilai_1 = 10;
+  $nilai_2 = 3;
+  $nilai_3 = 2 * $nilai_1 + 8 * $nilai_2;
+  echo "nilai = ", $nilai_3;
+  echo "<br>";
+  $jumlah = $nilai_1 + $nilai_2;
+  echo "hasil dari $nilai_1 + $nilai_2 adalah : $jumlah";
+  echo "<br><br>";
+  ?>
+</body>
+</html>
---
+++ praktek/mod_2.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+  <title> Pemograman PHP dengan Array</title>
+</head>
+<body>
+  <?php
+  // Array
+  $nama[] = "Issabela";
+  $nama[] = "Affan";
+  $nama[] = "Desty";
+  echo $nama[1] . $nama[2] . $nama[0];
+  echo "<br>";
+
+  // hitung jumlah array
+  $jum_array = count($nama);
+  echo "jumlah element array =". $jum_array;
+  
+  // bentuk lain
+  $kampus[sekolah] = "STMIK";
+  $kampus[nama_kampus] = "Akakom";
+  echo "Kampusku adalah $kampus[sekolah] $kampus[nama_kampus]";
+  ?>
+</body>
+</html>
