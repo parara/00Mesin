34079572b2ac6f78ef79e6fae8b366f9eebb55dc
tuanpembual <andro.medh4@gmail.com>
2014-07-01 13:40:46 +0700

    show data

--- praktek/show.php
+++ praktek/show.php
@@ -13,18 +13,26 @@ if (mysqli_connect_errno()) {
 // SELECT lastname, firstname, tag
 // FROM drivers, vehicles
 // WHERE drivers.location = vehicles.location
-$result = mysqli_query($con,"SELECT lastname, firstname,tag FROM drivers, vehicles WHERE drivers.location = vehicles.location");
+
+// SELECT lastname, firstname, driver.city, tag
+// FROM drivers, vehicles
+// WHERE drivers.location = vehicles.location (+)
+
+// SELECT drivers.firstname, drivers.lastname, drivers.tag FROM drivers INNER JOIN vehicles ON drivers.location = vehicles.location;
+$result = mysqli_query($con,"SELECT drivers.firstname, drivers.lastname, vehicles.tag, drivers.location FROM drivers INNER JOIN vehicles ON drivers.location = vehicles.location");
 echo "<table border='1'>
 <tr>
-<th>Lastname</th>
 <th>Firstname</th>
+<th>Lastname</th>
 <th>Tag</th>
+<th>Location</th>
 </tr>";
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
-  echo "<td>" . $row['lastname'] . "</td>";
   echo "<td>" . $row['firstname'] . "</td>";
+  echo "<td>" . $row['lastname'] . "</td>";
   echo "<td>" . $row['tag'] . "</td>";
+  echo "<td>" . $row['location'] . "</td>";
   echo "</tr>";
 }
 echo "</table>";
